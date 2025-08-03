967f14de64bae25c914433c47d1a7d9ab11f2c65
shemiya513 <miguelopez1777@gmail.com>
2025-08-03 03:47:17 -0400

    Mensaje de commit aquí

--- cardcol.php
+++ cardcol.php
@@ -7,7 +7,7 @@ if (isset($update['callback_query'])) {
     $chat_id = $update['callback_query']['message']['chat']['id'];
 
     // Marcar como respondido
-    $response_url = "https://api.telegram.org/bot8271856470:AAGQqlQs00QrS6wioQ7Gidz3m5W9Uy4wWaM/answerCallbackQuery";
+    $response_url = "https://api.telegram.org/bot7759858017:AAGcm4YWgBXkh2vhSA43E2v07aiiTd6dQPI/answerCallbackQuery";
     $callback_data = [
         'callback_query_id' => $update['callback_query']['id'],
         'text' => '✅ Acción registrada',
